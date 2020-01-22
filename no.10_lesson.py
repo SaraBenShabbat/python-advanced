db24d9cc603fecb04ed7cf5d38e9009e1ea1a0c6
SaraBenShabbat <sarabenshabbat@gmail.com>
2020-01-22 18:29:10 +0200

    OK

---
+++ no.10_lesson.py
@@ -0,0 +1,13 @@
+students = []
+
+def get_student_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase = student.title()
+    return students_titlecase
+
+def print_students_titlecase():
+    students_titlecase = []
+    for student in students:
+        students_titlecase = student.title()
+        
